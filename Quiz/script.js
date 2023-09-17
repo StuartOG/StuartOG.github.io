3b7fe665af97a29ceff746de426538d34d1d79f1
StuartOG <otisstu1@gmail.com>
2023-09-17 13:29:43 +0100

    Update

---
+++ Quiz/script.js
@@ -0,0 +1,20 @@
+const question = [
+    {
+        question: "What is 4x4?",
+        answers: [
+            {text: "8", correct: false},
+            {text: "16", correct: true},
+            {text: "20", correct: false},
+            {text: "44", correct: false},
+        ]
+    },
+    {
+        question: "What is 4x4?",
+        answers: [
+            {text: "8", correct: false},
+            {text: "16", correct: true},
+            {text: "20", correct: false},
+            {text: "44", correct: false},
+        ]
+    }
+]
